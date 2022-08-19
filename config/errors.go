608ffc2bd5f209b1c7dc0c56146d37e94f43f547
twothicc <chenenliuu@gmail.com>
2022-08-19 11:28:59 +0800

    setup toml configs

---
+++ config/errors.go
@@ -0,0 +1,12 @@
+package config
+
+import (
+	"github.com/twothicc/common-go/errortype"
+)
+
+const pkg = "config"
+
+var (
+	ErrParse    = errortype.ErrorType{Code: 1, Pkg: pkg}
+	ErrNotFound = errortype.ErrorType{Code: 2, Pkg: pkg}
+)
